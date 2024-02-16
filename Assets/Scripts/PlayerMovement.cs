857b1da990f0de7452eb245985175f95cf85f1a6
Alfonsosh02 <alfonsohenao08@gmail.com>
2024-02-16 13:01:59 +0100

    UPKEY

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public float velocity;
+    public KeyCode upKey;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey))
+        {
+            transform.Translate(Vector2.up * velocity * Time.deltaTime);
+        }
+    }
+}
